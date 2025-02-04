e684620898b5f769ae6faa4bed648102386475d2
RRARI504 <ivorysimien56@gmail.com>
2025-02-04 14:27:26 -0600

    17

--- function-master.js
+++ function-master.js
@@ -75,7 +75,8 @@ function arrayOrObject(collection) {
 
 function capitalizeWord(string) {
 
-    
+    return string.charAt(0).toUpperCase() + string.slice(1)
+
 }
 
 //////////////////////////////////////////////////////////////////////
